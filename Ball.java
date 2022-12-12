d2e5d69879b3c13bf50b319a5de559feb9827299
kirill160 <81866092+kirill160@users.noreply.github.com>
2022-12-12 14:19:41 +0300

    pong Libgdx game

---
+++ Ball.java
@@ -0,0 +1,11 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.TextureData;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Ball {
+
+}
